dfe729f6fa7259c4b567433212de47210846d4be
Artı Max Grubu <91283705+Proalbert@users.noreply.github.com>
2024-01-08 10:32:04 +0200

    Anket Porgramı Giriş Ekranı
    Anket programı giriş ekranı node.js ile yaptığım.

---
+++ NODE.JS/index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs=require('fs');
+
+
+var server = http.createServer(function(req,res) {
+   fs.readFile('index.html', function(err,data) {
+
+    res.write(data);
+
+    res.end();
+
+   });
+
+
+
+
+
+});
+
+
+
+
+
+server.listen(8000);
